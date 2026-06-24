0576a3ebefaebf8f92735e3e55e8a336789e6cd0
ottobot2202 <ottobot2202@gmail.com>
2026-06-24 19:12:37 -0400

    We are fix pistol gun and alsossssssssssssssso draw hitscan cool line yaya :D
    giasfelfbreber

--- Assets/Scripts/UI/ItemFiles/Pistol Item.cs
+++ Assets/Scripts/UI/ItemFiles/Pistol Item.cs
@@ -1,26 +1,33 @@
 using UnityEngine;
+using System.Collections;
 
 public class PistolItem : Item, IInteractable
 {
     private int cooldown;
-    public GameObject Player;
+    public GameObject player;
+    LineRenderer bulletLine = GetComponent<LineRenderer>();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        player = GameObject.FindWithTag("Player");
         setName("Pistol");
-        setAmmo(ammoTypes.Pistol); 
+        setAmmo(ammoTypes.Pistol);
     }
 
     public override void Use1()
     {
         if (cooldown == 0) 
         {
-            Ray detector = new Ray(Player.transform.position, Player.transform.forward);
+
+           
+
+            Ray detector = new Ray(player.transform.position, player.transform.forward);
+            Debug.DrawRay(player.transform.position, player.transform.forward * 100, Color.yellow, 1f);
             Debug.Log("shots fired!");
 
 
-
+            
 
             if (Physics.Raycast(detector,out RaycastHit hit,100))
             {
@@ -31,8 +38,19 @@ public class PistolItem : Item, IInteractable
                     Debug.Log("Pistol Hit an enemy");
                     EnemyHealth HP = (hit.collider.GetComponent<EnemyHealth>());
                     HP.Health -= 10;
+
+                    StartCoroutine(ShowTracer(hit.collider.transform.position));
                 }
             }
         }
+    }
+
+    IEnumerator ShowTracer(Vector3 hitPosition)
+    {
+        bulletLine.SetPosition(0, transform.position);
+        bulletLine.SetPosition(1, hitPosition);
+        bulletLine.enabled = true;
+        yield return new WaitForSeconds(0.1f);
+        bulletLine.enabled = false;
     }
 }
